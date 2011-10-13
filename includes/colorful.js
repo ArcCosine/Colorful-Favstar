ddef9f8296064cdf7f812f7cf2b33351f7624231
Arc Cosine <m.ksdcom@gmail.com>
2011-10-14 03:42:19 +0900

    コピー漏れを追加

--- includes/colorful.js
+++ includes/colorful.js
@@ -9,62 +9,67 @@
 // @include        http://es.favstar.fm/*
 // ==/UserScript==
 (function (_d,_w) {
-  function init(){
 
-    var path = 'styles/favstarlike.css';
-    var onCSS = function(event) {
-        var message = event.data;
+	function insertCSS(){
 
-        // Check this is the correct message and path from the background script.
-        if (message.topic === 'LoadedInjectedCSS' && message.data.path === path) {
-            // Remove the message listener so it doesn't get called again.
-            opera.extension.removeEventListener('message', onCSS, false);
+		var path = 'styles/favstarlike.css';
+		var onCSS = function(event) {
+			var message = event.data;
 
-            var css = message.data.css;
+			// Check this is the correct message and path from the background script.
+			if (message.topic === 'LoadedInjectedCSS' && message.data.path === path) {
+				// Remove the message listener so it doesn't get called again.
+				opera.extension.removeEventListener('message', onCSS, false);
 
-            // Create a <style> element and add it to the <head> element of the current page.
-            // Insert the contents of the stylesheet into the <style> element.
-            var style = _d.createElement('style');
-            style.setAttribute('type', 'text/css');
-            style.appendChild(_d.createTextNode(css));
-            _d.getElementsByTagName('head')[0].appendChild(style);
-        }
-    }
+				var css = message.data.css;
 
-    // On receipt of a message from the background script, execute onCSS().
-    opera.extension.addEventListener('message', onCSS, false);
+				if( !widget.preferences.adsSelector ){
+					widget.preferences.adsSelector = "#bonusFeatureCallout,#signInCallout, .friends_with_bonus";
+				}
 
-    // Send the stylesheet path to the background script to get the CSS.
-    opera.extension.postMessage({
-        topic: 'LoadInjectedCSS',
-        data: path
-    });
+				var addingCSS = widget.preferences.adsSelector + " { display: none !important; }";
 
-    var favCounts = _d.querySelectorAll('.count');
-    for (var i=favCounts.length; i-- > 0; ){
-      var fav = parseInt(favCounts[i].innerHTML, 10);
-      var pnode = favCounts[i].parentNode;
-      while(pnode){
-        if( pnode.className.indexOf('tweetWithStats') >= 0 ){
-          break;
-        }
-        pnode = pnode.parentNode;
-      }
-      if (!isNaN(fav) && fav >= 1 && pnode ) {
-        setColorfulClass(fav, pnode);
-      }
-    }
-  }
+				css += addingCSS;
 
-  function setColorfulClass (favCount, node) {
-    if (node.className !== 'tweetWithStats') {return;}
-    var tweet = node.querySelector('.theTweet');
-    if (favCount <= 4) {
-      tweet.className += ' cofav'+favCount;
-    } else if (favCount >= 5) {
-      tweet.className += ' cofav5';
-    }
-  }
 
-  _d.addEventListener('DOMContentLoaded', function(){ init(); }, false );
+				// Create a <style> element and add it to the <head> element of the current page.
+				// Insert the contents of the stylesheet into the <style> element.
+				var style = _d.createElement('style');
+				style.setAttribute('type', 'text/css');
+				style.appendChild(_d.createTextNode(css));
+				_d.getElementsByTagName('head')[0].appendChild(style);
+
+			}
+		}
+
+		// On receipt of a message from the background script, execute onCSS().
+		opera.extension.addEventListener('message', onCSS, false);
+
+		// Send the stylesheet path to the background script to get the CSS.
+		opera.extension.postMessage({
+			topic: 'LoadInjectedCSS',
+			data: path
+		});
+
+
+	}
+
+	function init(){
+		var tweetNodes = _d.querySelectorAll('.theTweet');
+		for (var i=tweetNodes.length; i-- > 0; ){
+			var pnode = tweetNodes[i].parentNode.parentNode.parentNode;	//.tweetWithStats
+			var favNode = pnode.querySelector(".count");
+			var fav = parseInt(favNode.innerHTML, 10);
+			if (!isNaN(fav) && fav >= 1) {
+				if( fav > 5 ){
+					fav = 5;
+				}
+				tweetNodes[i].className += ' cofav'+fav;
+			}
+		}
+	}
+
+	insertCSS();
+
+	_w.addEventListener('DOMContentLoaded', init, false );
 })(document, window);
