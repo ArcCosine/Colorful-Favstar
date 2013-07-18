824729838172e7b5e44c9c11e3fbd428cbe1dd68
ArcCosine <m.ksdcom@gmail.com>
2013-07-19 01:17:07 +0900

    Update new Opera's version.

--- includes/colorful.js
+++ includes/colorful.js
@@ -2,54 +2,13 @@
 // @name           colorful_favstar
 // @namespace      http://looxu.blogspot.com/
 // @description    Change favstar text like fabotter.
-// @version        1.3
+// @version        2.0
 // @include        http://favstar.fm/*
 // @include        http://ja.favstar.fm/*
 // @include        http://de.favstar.fm/*
 // @include        http://es.favstar.fm/*
 // ==/UserScript==
 (function (_d,_w) {
-
-	function insertCSS(){
-
-		var path = 'styles/favstarlike.css';
-		var onCSS = function(event) {
-			var message = event.data;
-
-			// Check this is the correct message and path from the background script.
-			if (message.topic === 'LoadedInjectedCSS' && message.data.path === path) {
-				// Remove the message listener so it doesn't get called again.
-				opera.extension.removeEventListener('message', onCSS, false);
-
-				var css = message.data.css;
-
-				if( !widget.preferences.adsSelector ){
-					widget.preferences.adsSelector = '#bonusFeatureCallout,#signInCallout, .friends_with_bonus';
-				}
-				css += widget.preferences.adsSelector + ' { display: none !important; }';
-
-
-				// Create a <style> element and add it to the <head> element of the current page.
-				// Insert the contents of the stylesheet into the <style> element.
-				var style = _d.createElement('style');
-				style.setAttribute('type', 'text/css');
-				style.appendChild(_d.createTextNode(css));
-				_d.getElementsByTagName('head')[0].appendChild(style);
-
-			}
-		}
-
-		// On receipt of a message from the background script, execute onCSS().
-		opera.extension.addEventListener('message', onCSS, false);
-
-		// Send the stylesheet path to the background script to get the CSS.
-		opera.extension.postMessage({
-			topic: 'LoadInjectedCSS',
-			data: path
-		});
-
-
-	}
 
 	function init(){
 
@@ -74,10 +33,7 @@
 			}
 		}
 	}
-
-	insertCSS();
-
-	_w.addEventListener('DOMContentLoaded', init, false );
+	init();
 
 })(document, window);
 
